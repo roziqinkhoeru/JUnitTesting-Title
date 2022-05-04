e8a5f6afe7929d93f11174518d165aca5141b03e
roziqinkhoeru <roziqinkhoeru8@gmail.com>
2022-05-04 16:03:01 +0700

    initial create test

---
+++ junit5-temperature-converter/src/test/java/junit5/informatics6/FahrenheitConverterTest.java
@@ -0,0 +1,14 @@
+package junit5.informatics6;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class FahrenheitConverterTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
