d6c8ff9e5228463d9a858370ad46a8e917c99558
roziqinkhoeru <roziqinkhoeru8@gmail.com>
2022-05-04 14:33:49 +0700

    Create Class TemperatureConverter

---
+++ junit5-temperature-converter/src/main/java/junit5/informatics6/TemperatureConverter.java
@@ -0,0 +1,18 @@
+package junit5.informatics6;
+
+public class TemperatureConverter {
+	// celcius to reamur
+	public double celciusToReamur(double c) {
+		return 4 * c / 5;
+	}
+
+	// celcius to fahrenheit
+	public double celciusToFahrenheit(double c) {
+		return (9 * c / 5) + 32;
+	}
+
+	// celcius to kelvin
+	public double celciusToKelvin(double c) {
+		return c + 273.15;
+	}
+}
